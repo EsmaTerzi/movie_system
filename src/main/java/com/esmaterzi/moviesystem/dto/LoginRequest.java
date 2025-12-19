a39a6dac44c7bbf9ba30d90a4b5fbf461b896dfe
EsmaTerzi <n.esmaterzi@gmail.com>
2025-12-19 22:12:39 +0300

    pom.xml changed last manuel dto.

--- src/main/java/com/esmaterzi/moviesystem/dto/LoginRequest.java
+++ src/main/java/com/esmaterzi/moviesystem/dto/LoginRequest.java
@@ -1,15 +1,39 @@
 package com.esmaterzi.moviesystem.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class LoginRequest {
+
     @NotBlank(message = "Username is required")
     private String username;
 
     @NotBlank(message = "Password is required")
     private String password;
+
+    // 1. Parametresiz (Boş) Constructor - Jackson için şart!
+    public LoginRequest() {
+    }
+
+    // 2. Parametreli Constructor - Testler için kolaylık olur
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // 3. Getter ve Setter'lar - Manuel olarak ekledik
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
